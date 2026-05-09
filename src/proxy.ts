148863e3708d4c499b38da04a594b37e032b0c96
Eslam-Elngar <eslamelngar555@gmail.com>
2026-05-09 14:37:12 +0300

    proxy not middleware

--- src/proxy.ts
+++ src/proxy.ts
@@ -2,7 +2,7 @@ import { createServerClient } from '@supabase/ssr';
 import { NextResponse, type NextRequest } from 'next/server';
 
 // لازم اسم الـ Function يكون middleware
-export async function middleware(request: NextRequest) {
+export async function proxy(request: NextRequest) {
   let supabaseResponse = NextResponse.next({
     request,
   });
