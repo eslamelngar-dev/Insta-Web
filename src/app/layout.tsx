dadb9980d08879226dc5be89e49673967c3f7b1a
Eslam-Elngar <eslamelngar555@gmail.com>
2026-05-09 20:07:39 +0300

    not found page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,9 @@
-import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { Toaster } from "sonner";
 import { ThemeProvider } from "@/components/ThemeProvider";
+import { Toaster } from "sonner";
 
 const inter = Inter({ subsets: ["latin"] });
-
-export const metadata: Metadata = {
-  title: "InstaWeb",
-  description: "Build your digital identity",
-};
 
 export default function RootLayout({
   children,
@@ -17,10 +11,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
+    // suppressHydrationWarning ضرورية هنا لأن الـ ThemeProvider يغير الـ class
     <html lang="en" suppressHydrationWarning>
-      <body className={inter.className}>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-          {children}
+      <body className={inter.className} suppressHydrationWarning>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <div className="relative flex min-h-screen flex-col">
+            {children}
+          </div>
           <Toaster position="bottom-right" richColors />
         </ThemeProvider>
       </body>
